626b0b9fdfbb7da15b7f0f810c14e70b8d35a974
ismail <ismaileski@gmail.com>
2018-02-21 16:30:54 +0300

    add scrollbar

--- AutoComplete.js
+++ AutoComplete.js
@@ -1,122 +1,135 @@
-Vue.component("autocomplete", {
-    template:
-`
-                <div class="dropdown">
-                    <div class="input-group" >
-                        <input type="text" placeholder="Aranacak metin..." v-model="searchTerm" class="form-control" @input="searchValue()" @keydown="handleKeyDown" />
-                        <span class="input-group-addon">
-                            <span v-bind:class="okClass" aria-hidden="true"></span>
-                        </span>
-                    </div>
+    Vue.component("autocomplete",
+        {
+            template: "#autocomplete",
+            props: {
+                url: { type: String, required: true },
 
-                    <ul class="dropdown-menu" style="position: relative" v-bind:style="{ display: display }"  >
-                        <li v-for="(item, index) in items" :class="activeClass(index)" v-bind:id="item.id" v-on:click="selectItem(index)" @keydown="handleKeyDown" @mousemove="hoverItem(index)">
-                            <a>{{item.adi}}</a>
-                        </li>
-                    </ul>
-                </div>
-                `,
-    props: ["url", "parentevent", "id", "value"],
-    data: function () {
-        return {
-            selectedItem: { },
-            okClass:'glyphicon glyphicon-search',
-            display: "none",
-            focusItem: 0,
-            searchTerm: "",
-            items: []
-        }        
+                id: { type: String, required: true },
 
-    },
-    methods: {
-        handleKeyDown(e) {
+                parentevent: { type: Function, required: false },
 
-            if (this.items.length == 0) return;
+                placeholdervalue: { type: String, required: true },
 
-            var key = e.keyCode;
+                classname: { type: String, required: true },
 
-            // Key List
-            const DOWN = 40;
-            const UP = 38;
-            const ENTER = 13;
-            const ESC = 27;
+                pid: { type: String, required: true },
+                pvalue: { type: String, required: true },
 
-            if (
-                (key == UP && this.focusItem <= 0) ||
-                (key == DOWN && this.focusItem >= this.items.length-1)
-            ) {
-                e.preventDefault();
-                return;
-            }
+                value: { type: String, required: true }
+            },
+            data: function () {
+                return {
+                    islemId: "",
+                    items: [],
+                    selectedItem: {},
 
-            switch (key) {
-                case DOWN:
-                    e.preventDefault();
-                    this.focusItem++;
-                    break;
-                case UP:
-                    e.preventDefault();
-                    this.focusItem--;
-                    break;
-                case ENTER:
-                    e.preventDefault()
-                    this.display = "none";
-                    if (this.items.length > 0)
-                        this.selectItem(this.focusItem);
+                    display: "none",
 
-                    break;
-                case ESC:
-                    break;
-            }
+                    focusItem: 0,
+                    searchTerm: ""
+                }
+            },
+            created: function () {
+                this.searchTerm = this.value;
+            },
+            methods: {
+                handleKeyDown(e) {
 
-        },
-        activeClass(i) {
-            const focusClass = i === this.focusItem ? 'active' : "";
-            return `${focusClass}`;
-        },
-        hoverItem(index) {
-            this.focusItem = index;
-        },
-        selectItem: function (index) {
-            this.selectedItem = this.items[index];
+                    if (this.items.length == 0) return;
 
-            this.searchTerm = this.selectedItem[this.value];
+                    var key = e.keyCode;
 
-            this.items = [];
-            this.focusItem = -1;
+                    // Key List
+                    const DOWN = 40;
+                    const UP = 38;
+                    const ENTER = 13;
+                    const ESC = 27;
 
-            this.$emit("parentevent", this.selectedItem);
-            this.display = "none";
-            this.okClass = "glyphicon glyphicon-ok";
-
-        },
-        searchValue: function () {
-
-            this.okClass = "glyphicon glyphicon-search";
-
-            this.display = "none";
-            this.items = [];
-
-            if (this.searchTerm.length < 3) return;
-
-            const that = this;
-
-            window.axios.get(this.url, { params: { value: this.searchTerm } })
-                .then(function (response) {
-
-                    if (response.data.length == 0) {
-                        that.display = "none";
+                    if (
+                        (key == UP && this.focusItem <= 0) ||
+                        (key == DOWN && this.focusItem >= this.items.length - 1)
+                    ) {
+                        e.preventDefault();
                         return;
                     }
 
-                    that.items = response.data;
-                    that.display = "inline-block";
-                    that.focusItem = 0;
-                })
-                .catch(function (error) {
-                    alert(error);
-                });
+                    var container = this.$el.querySelector("#container");
+
+                    switch (key) {
+                        case DOWN:
+                            e.preventDefault();
+                            this.focusItem++;
+                            break;
+                        case UP:
+                            e.preventDefault();
+                            this.focusItem--;
+                            break;
+                        case ENTER:
+                            e.preventDefault();
+                            this.display = "none";
+                            if (this.items.length > 0)
+                                this.selectItem(this.focusItem);
+                            break;
+                        case ESC:
+                            break;
+                    }
+                    container.scrollTop = container.children[0].clientHeight * this.focusItem;
+
+                },
+                activeClass(i) {
+                    const focusClass = i === this.focusItem ? 'active' : "";
+                    return `${focusClass}`;
+                },
+                hoverItem(index) {
+                    this.focusItem = index;
+                },
+                selectItem: function (index) {
+                    this.selectedItem = this.items[index];
+
+                    this.searchTerm = this.selectedItem[this.pvalue];
+
+                    this.items = [];
+                    this.focusItem = -1;
+                    this.display = "none";
+
+                    this.$emit('parentevent', this.selectedItem);
+
+                },
+                searchValue: function () {
+                    this.display = "none";
+                    this.items = [];
+                    this.selectedItem = {};
+
+                    if (this.searchTerm.length == 0) {
+                        var emptyTtem = {};
+
+                        emptyTtem[this.pid] = "";
+                        emptyTtem[this.pvalue] = "";
+
+                        this.$emit('parentevent', emptyTtem);
+                    }
+                        
+
+                    if (this.searchTerm.length < 3) return;
+
+                    const that = this;
+
+                    window.axios.get(this.url, { params: { value: this.searchTerm } })
+                        .then(function (response) {
+
+                            if (response.data.length == 0) {
+                                that.display = "none";
+                                return;
+                            }
+
+                            that.items = response.data;
+                            that.display = "inline-block";
+                            that.focusItem = 0;
+                        })
+                        .catch(function (error) {
+                            alert("Hata Oluştu" + error);
+                        });
+                }
+            }
         }
-    }
-}
-);
+    );
