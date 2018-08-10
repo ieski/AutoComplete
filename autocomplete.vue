065d0bc92940ccd7065eb54990337679ff11bd49
ismail <ismaileski@gmail.com>
2018-08-10 23:24:42 +0300

    Update autocomplete.vue

--- autocomplete.vue
+++ autocomplete.vue
@@ -1,11 +1,17 @@
-<script type="text/x-template" id="autocomplete">
     <div class="dropdown">
-        <input autocomplete="off" type="text" :id="id" :name="id" v-model="searchTerm" :placeholder="placeholdervalue" :class="classname" @@input="searchValue()" @@keydown="handleKeyDown" />
+        <input :id="inputId" :name="inputId" type="hidden" v-model="id" />
+        <input autocomplete="off" :id="inputValueId" :name="inputValueId"
+               :placeholder="placeHolderValue"
+               :class="className"
+               :disabled="inputValueDisabled"
+               type="text"
+               v-model="value"
+               @@input="searchValue()"
+               @@keydown="handleKeyDown" />
 
-        <ul id="container" class="dropdown-menu" style="position: relative; z-index:1; overflow-y: auto; max-height:200px " v-bind:style="{ display: display}">
+        <ul id="container" class="dropdown-menu" style="z-index:1; overflow-y: auto; max-height:200px " v-bind:style="{ display: display}">
             <li v-for="(item, index) in items" :class="activeClass(index)" v-bind:id="item.id" v-on:click="selectItem(index)" @@keydown="handleKeyDown" @@mousemove="hoverItem(index)">
-                <a>{{item[pvalue]}}</a>
+                <a>{{item.adi}}</a>
             </li>
         </ul>
     </div>
-</script>
